e3dc1e509568ad8004c372bb55ca400d67c790a2
Caleb Gray <hey@calebgray.com>
2020-05-27 11:54:43 -0700

    Build!

--- .github/actions/psftp/build.sh
+++ .github/actions/psftp/build.sh
@@ -6,12 +6,10 @@ export GOARCH=amd64
 
 # Convert Icon
 for i in 1 2 3 4 5; do go get -u github.com/cratonica/2goarray && break || sleep 1; done
-while [ $? -ne 0 ]; do bash -c !!; done
 /root/go/bin/2goarray Icon main < icon.ico > icon.go
-
+s
 # Embed Icon in Resource
 for i in 1 2 3 4 5; do go get -u github.com/akavel/rsrc && break || sleep 1; done
-while [ $? -ne 0 ]; do bash -c !!; done
 /root/go/bin/rsrc -ico icon.ico
 
 # Before Building
