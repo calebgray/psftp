2ca4f01591bd75ee6cbc7819a77af85445407a6e
Caleb Gray <hey@calebgray.com>
2020-05-27 10:01:42 -0700

    Build!

--- .github/actions/psftp/build.sh
+++ .github/actions/psftp/build.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-# Setup Environment
-mkdir -p "/github/home/go/src/github.com/${GITHUB_ACTOR}"
-ln -s "${GITHUB_WORKSPACE}" "/github/home/go/src/github.com/${GITHUB_REPOSITORY}"
-cd "/github/home/go/src/github.com/${GITHUB_REPOSITORY}" || exit 1
-
 # Before Building
 export CC=i686-w64-mingw32-gcc
 
