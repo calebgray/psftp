bcf4afeeddf106aff8858f67257a86398d43d003
Caleb Gray <hey@calebgray.com>
2020-05-27 12:58:14 -0700

    Build!

--- .github/actions/psftp/build.sh
+++ .github/actions/psftp/build.sh
@@ -12,18 +12,17 @@ for i in 1 2 3 4 5; do go get -u github.com/cratonica/2goarray && break || sleep
 for i in 1 2 3 4 5; do go get -u github.com/akavel/rsrc && break || sleep 1; done
 ~/go/bin/rsrc -ico icon.ico
 
-# Before Building
-export GOOS=windows
-export GOARCH=386
-
 # Go Get Deps!
 go get -d . || true
 
 # Go Build!
+export GOOS=windows
+export GOARCH=386
 export CC=i686-w64-mingw32-gcc
 go build -ldflags "-linkmode=internal -H=windowsgui" .
+mv psftp.exe /psftp32.exe
 
-# List Files
-pwd
-echo '----'
-ls
+export GOARCH=amd64
+export CC=x86_64-w64-mingw32-gcc
+go build -ldflags "-linkmode=internal -H=windowsgui" .
+mv psftp.exe /psftp64.exe
