356f16920d3a8cecbe9c352e0b25958dc18cb3f0
Caleb Gray <hey@calebgray.com>
2020-05-27 11:01:57 -0700

    Build!

--- .github/actions/psftp/build.sh
+++ .github/actions/psftp/build.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 # Before Building
+export GOOS=windows
+export GOARCH=386
 export CC=i686-w64-mingw32-gcc
 
-# Fetch Go Dependencies
-go get -v ./... || true
+# Go Get Deps!
+rm -fr ~/go && go get -d .
+while [ $? -ne 0 ]; do !!; done
 
 # Go Build!
 go build .
