f2d4d46f2a8857bc3b77f86d920d05fc45b23aed
Caleb Gray <hey@calebgray.com>
2020-05-27 08:19:06 -0700

    Build...

--- .github/actions/psftp/build.sh
+++ .github/actions/psftp/build.sh
@@ -1,17 +1,17 @@
 #!/bin/sh
 
 # Setup Environment
-mkdir -p "/root/go/src/github.com/${GITHUB_ACTOR}"
-ln -s "${GITHUB_WORKSPACE}" "/root/go/src/github.com/${GITHUB_REPOSITORY}"
-cd "/root/go/src/github.com/${GITHUB_REPOSITORY}" || exit 1
+mkdir -p "/github/home/go/src/github.com/${GITHUB_ACTOR}"
+ln -s "${GITHUB_WORKSPACE}" "/github/home/go/src/github.com/${GITHUB_REPOSITORY}"
+cd "/github/home/go/src/github.com/${GITHUB_REPOSITORY}" || exit 1
 
 # Before the Build
 export CC=i686-w64-mingw32-gcc
 
 # Fetch Go Dependencies
-go get -v ./...
+go get -v ./... || true
 
 # Go Build!
-mkdir /root/go/src/github.com/ffred && cd /root/go/src/github.com/ffred && git clone https://github.com/ffred/guitocons
-mkdir -p /root/go/src/golang.org/x && cd /root/go/src/golang.org/x && git clone https://github.com/golang/sys
+mkdir /github/home/go/src/github.com/ffred && cd /github/home/go/src/github.com/ffred && git clone https://github.com/ffred/guitocons
+mkdir -p /github/home/go/src/golang.org/x && cd /github/home/go/src/golang.org/x && git clone https://github.com/golang/sys
 GOOS=windows GOARCH=386 go build .
