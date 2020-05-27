5ede47cbc6186d2ab3742ca410953344c65184a9
Caleb Gray <hey@calebgray.com>
2020-05-27 08:24:55 -0700

    Build...

--- .github/actions/psftp/build.sh
+++ .github/actions/psftp/build.sh
@@ -5,13 +5,13 @@ mkdir -p "/github/home/go/src/github.com/${GITHUB_ACTOR}"
 ln -s "${GITHUB_WORKSPACE}" "/github/home/go/src/github.com/${GITHUB_REPOSITORY}"
 cd "/github/home/go/src/github.com/${GITHUB_REPOSITORY}" || exit 1
 
-# Before the Build
+# Before Building
 export CC=i686-w64-mingw32-gcc
 
 # Fetch Go Dependencies
 go get -v ./... || true
+mkdir /github/home/go/src/github.com/ffred && cd /github/home/go/src/github.com/ffred && git clone https://github.com/ffred/guitocons
+ln -s /github/home/go/src/golang.org /github/home/go/src/golang.org/x
 
 # Go Build!
-mkdir /github/home/go/src/github.com/ffred && cd /github/home/go/src/github.com/ffred && git clone https://github.com/ffred/guitocons
-mkdir -p /github/home/go/src/golang.org/x && cd /github/home/go/src/golang.org/x && git clone https://github.com/golang/sys
 GOOS=windows GOARCH=386 go build .
