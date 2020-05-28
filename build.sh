38320be9fd6295e57e3edf3413820a39485d46ce
Caleb Gray <hey@calebgray.com>
2020-05-28 12:28:49 -0700

    Build!

--- build.sh
+++ build.sh
@@ -35,14 +35,21 @@ go get -u github.com/akavel/rsrc || su -c "!!" || exit 40
 ~/go/bin/rsrc -ico icon.ico
 
 # Go Build!
-GOOS=windows GOARCH=386 CC=i686-w64-mingw32-gcc go build -ldflags '-linkmode=internal -H=windowsgui' . && mv psftp.exe /psftp32.exe || exit 50
-GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc go build -ldflags '-linkmode=internal -H=windowsgui' . && mv psftp.exe /psftp64.exe || exit 51
-#GOOS=linux GOARCH=386 CC="gcc -m32 -melf_i386" go build -ldflags '-linkmode=internal' . && mv psftp /psftp32 || exit 60
-GOOS=linux GOARCH=amd64 CC="gcc -m64" go build -ldflags '-linkmode=internal' . && mv psftp /psftp64 || exit 61
+mkdir build
+cp ./*.md build
+GOOS=windows GOARCH=386 CC=i686-w64-mingw32-gcc go build -ldflags '-linkmode=internal -H=windowsgui' . && mv psftp.exe build/psftp32-${PROJECT_VERSION}.exe || exit 50
+GOOS=windows GOARCH=amd64 CC=x86_64-w64-mingw32-gcc go build -ldflags '-linkmode=internal -H=windowsgui' . && mv psftp.exe build/psftp64-${PROJECT_VERSION}.exe || exit 51
+#GOOS=linux GOARCH=386 CC="gcc -m32 -melf_i386" go build -ldflags '-linkmode=internal' . && mv psftp build/psftp32-${PROJECT_VERSION}-gtk || exit 60
+GOOS=linux GOARCH=amd64 CC="gcc -m64" go build -ldflags '-linkmode=internal' . && mv psftp build/psftp64-${PROJECT_VERSION}-gtk || exit 61
 
 # Exports
 if [ "${GITHUB_ACTIONS}" == 'true' ]; then
-	echo "::set-env name=OUTPUT_NAMES::$(ls /psftp*)"
+	export BUILD_FILES=$(find build)
+	BUILD_FILES="${BUILD_FILES//'%'/'%25'}"
+	BUILD_FILES="${BUILD_FILES//$'\n'/'%0A'}"
+	BUILD_FILES="${BUILD_FILES//$'\r'/'%0D'}"
+	echo "::set-env name=BUILD_FILES::${BUILD_FILES}"
+	echo "::set-env name=BUILD_DIR::$(pwd)/build"
 fi
 
 # Debugging?
